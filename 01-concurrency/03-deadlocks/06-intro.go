73fa4b766b87d3aabbdd69b2f1595ef9e873ec92
Magesh Kuppan <tkmagesh77@gmail.com>
2024-11-04 16:38:55 +0530

    simulating deadlock

---
+++ 01-concurrency/03-deadlocks/06-intro.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(5) // increment the counter by 5
+	go f1(wg) // scheduling the f1() execution through the scheduler to be scheduled to execute in future
+	f2()
+	wg.Wait() //block until the wg counter becomes 0 (default = 0) (forever, and hence the scheduler reports a 'deadlock')
+}
+
+func f1(wg *sync.WaitGroup) {
+	defer wg.Done() // decrement the counter by 1
+	fmt.Println("f1 started")
+	time.Sleep(3 * time.Second)
+	fmt.Println("f1 completed")
+}
+
+func f2() {
+	fmt.Println("f2 invoked")
+}
