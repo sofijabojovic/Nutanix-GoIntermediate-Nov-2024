71ca35c7718d8bc26361ee794dcb40df150bfa6f
Magesh Kuppan <tkmagesh77@gmail.com>
2024-11-04 17:16:54 +0530

    exercise solution

--- 01-concurrency/05-communication/11-example.go
+++ 01-concurrency/05-communication/11-example.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
+// communicate by sharing memory (not advisable)
 var result int
 
 func main() {
-	go add(100, 200)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go add(100, 200, wg)
+	wg.Wait()
 	fmt.Println("Result :", result)
 }
 
-func add(x, y int) {
+func add(x, y int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	result = x + y
 }
